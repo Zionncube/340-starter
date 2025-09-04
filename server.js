f3e2d4b24630a7712cd4e6888e33d928bdb9814f
Zionncube <mercy.msawenkosi@gmail.com>
2025-09-04 16:40:51 +0200

    fix typo in commit message

--- server.js
+++ server.js
@@ -5,44 +5,41 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
- * view engine and templates
+ * View engine and templates
  *************************/
-
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
-const host = process.env.HOST
+const HOST = process.env.HOST || "localhost"; // optional, for local testing
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on ${HOST}:${PORT}`);
+});
 
+/* ***********************
+ * Home Route
+ *************************/
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
