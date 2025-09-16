fe3425aeefbe7e22351e068e80639a8494e9dab8
Zionncube <mercy.msawenkosi@gmail.com>
2025-09-16 15:46:48 +0200

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
-const baseController = require("./controllers/baseController")
+const baseController = require("./controllers/baseController");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5500;
 // EJS setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,11 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-
 // Routes
-app.get("/", baseController.buildHome)
+app.get("/", baseController.buildHome);
 
-
-app.listen(PORT, () => {
-  console.log(`✅ App listening on http://localhost:${PORT}`);
+// Start server - IMPORTANT: Listen on 0.0.0.0 for Render
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ App listening on port ${PORT}`);
 });
--- utilities/index.js
+++ utilities/index.js
@@ -22,3 +22,4 @@ Util.getNav = async function (req, res, next) {
 }
 
 module.exports = Util
+
