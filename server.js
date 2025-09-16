cb15972fe4252dfd3aef897a5207927234557438
Zionncube <mercy.msawenkosi@gmail.com>
2025-09-16 13:01:55 +0200

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const baseController = require("./controllers/baseController")
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,10 +13,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
+
 // Routes
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
-});
+app.get("/", baseController.buildHome)
+
 
 app.listen(PORT, () => {
   console.log(`✅ App listening on http://localhost:${PORT}`);
